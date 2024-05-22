8f9d764735f762fccab1eb1e3bb5f3de22774a56
Justin Garcia <justin.garcia@gmail.com>
2024-05-22 10:11:21 +0900

    clean up

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,21 +2,18 @@ require 'github/markup'
 # require 'youtube-captions'
 
 def transform_bracketed_text(markdown)
-  # Define a regex pattern to match text inside brackets
+  # Define a regex pattern to match text not inside brackets
   pattern = /([^\[]+)(?:\[[^\]]*?\])*/m
-  # Use gsub to find and transform all bracketed text
+  # Use gsub to find and transform all non-bracketed text
   markdown = markdown.gsub(pattern) do |match|
     GitHub::Markup.render_s(GitHub::Markups::MARKUP_MARKDOWN, match)
   end
 
+  # Now iterate over all bracketed text
   pattern = /\[(.*?)\]/m
-
   markdown = markdown.gsub(pattern) do |match|
-    # Capture the text inside the brackets
     inner_text = match[1..-2]
-    # Apply the transformation (uppercase in this case)
     transformed_text = "$$\\begin{gather}" + inner_text + "\\\\ \\notag \\end{gather}$$"
-    # Return the transformed text inside brackets
     transformed_text
   end
 
@@ -33,6 +30,7 @@ class NotesController < ApplicationController
       transcript = TranscriptGenerator.new(video_url).call
       memo = NoteGenerator.new(transcript).call
       note.memo = transform_bracketed_text(memo)
+      # fix me: use save here, not save!
       note.save!
     end
     redirect_to note_path(note)
