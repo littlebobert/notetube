89cf3c10e09072187e1bc461d528b10fe56128f2
Justin Garcia <justin.garcia@gmail.com>
2024-05-22 16:36:26 +0900

    stimulus read-more

---
+++ vendor/javascript/@stimulus-components--read-more.js
@@ -0,0 +1,2 @@
+import{Controller as e}from"@hotwired/stimulus";const t=class _ReadMore extends e{connect(){this.open=!1}toggle(e){this.open===!1?this.show(e):this.hide(e)}show(e){this.open=!0;const t=e.target;t.innerHTML=this.lessTextValue,this.contentTarget.style.setProperty("--read-more-line-clamp","'unset'")}hide(e){this.open=!1;const t=e.target;t.innerHTML=this.moreTextValue,this.contentTarget.style.removeProperty("--read-more-line-clamp")}};t.targets=["content"],t.values={moreText:String,lessText:String};let s=t;export{s as default};
+
