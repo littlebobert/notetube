66cb273a690409bb05b4c6d848503b2ee3d53522
Justin Garcia <justin.garcia@gmail.com>
2024-05-22 10:09:30 +0900

    clean up

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,25 +4,18 @@ require 'github/markup'
 def transform_bracketed_text(markdown)
   # Define a regex pattern to match text inside brackets
   pattern = /([^\[]+)(?:\[[^\]]*?\])*/m
-  puts "inside tranformed_bracketed_text: #{markdown}"
   # Use gsub to find and transform all bracketed text
   markdown = markdown.gsub(pattern) do |match|
-    puts "no match: #{match}"
     GitHub::Markup.render_s(GitHub::Markups::MARKUP_MARKDOWN, match)
   end
 
   pattern = /\[(.*?)\]/m
 
   markdown = markdown.gsub(pattern) do |match|
-    puts "hello. match: #{match}"
     # Capture the text inside the brackets
     inner_text = match[1..-2]
-    puts "hello. inner_text: #{inner_text}"
     # Apply the transformation (uppercase in this case)
     transformed_text = "$$\\begin{gather}" + inner_text + "\\\\ \\notag \\end{gather}$$"
-    puts ">"
-    p transformed_text
-    puts "<"
     # Return the transformed text inside brackets
     transformed_text
   end
@@ -39,7 +32,6 @@ class NotesController < ApplicationController
       note = Note.new(video_url: video_url, user: current_user)
       transcript = TranscriptGenerator.new(video_url).call
       memo = NoteGenerator.new(transcript).call
-      puts ">>>"
       note.memo = transform_bracketed_text(memo)
       note.save!
     end
