7845bd9ac9974973962a793385a8202846db38e5
ShuxingFang <151457729+ShuxingFang@users.noreply.github.com>
2024-05-22 15:56:05 +0900

    bookmark_page now works, but no css (#10)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_20_051648) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_22_062623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_20_051648) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "video_id"
+    t.string "thumbnail_url"
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
