c635ae04c819d3c5cb66182851fb0176dd3920e6
Justin Garcia <justin.garcia@gmail.com>
2024-05-23 13:40:56 +0900

    devise sign up fix

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,5 @@
+class RegistrationsController < Devise::RegistrationsController
+  def index
+    raise NoMethodError, "This method is defined just because otherwise we'll error visiting #home we do after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?"
+  end
+end
