6ad3d818d38ce2f5630850cd6dbe8e5263874d9d
ShuxingFang <151457729+ShuxingFang@users.noreply.github.com>
2024-05-23 15:58:24 +0900

    I wrote a beautify transcript method (#32)
    * I wrote a beautify transcript method
    
    * debug puts removed

--- app/service/transcript_generator.rb
+++ app/service/transcript_generator.rb
@@ -8,6 +8,35 @@ class TranscriptGenerator
     @url = url
   end
 
+  def beautify_transcript(ugly_transcript)
+    api_key = ENV["OPEN_AI_API_KEY"]
+    url = "https://api.openai.com/v1/chat/completions"
+
+    headers = {
+      'Content-Type' => 'application/json',
+      'Authorization' => "Bearer #{api_key}"
+    }
+
+    prompt = "Reformat the following transcript into paragraphs and add proper capitalization without changing the original words:\n\n#{ugly_transcript}"
+    # puts "OpenAI prompt: #{prompt}"
+
+    # Set up the request body
+    body = {
+      model: 'gpt-4o',
+      messages: [
+        { "role": "user", "content": prompt }
+      ],
+      max_tokens: 4096
+    }.to_json
+
+    response = HTTP.headers(headers).post(url, body: body)
+    response_data = JSON.parse(response.body.to_s)
+    puts response_data
+    final = response_data["choices"].first["message"]["content"]
+    puts "output: #{final}"
+    return final
+  end
+
   def call
     video = YoutubeCaptions::Video.new(id: @url)
     p video.info
@@ -17,6 +46,7 @@ class TranscriptGenerator
     captions.each do |caption|
       result = result + " " + caption["__content__"] unless caption["__content__"].nil?
     end
+
     return result
   end
 end
