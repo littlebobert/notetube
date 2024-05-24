810c05d9dc49d7991bfe026fd6acd85c1c0d239c
Justin Garcia <justin.garcia@gmail.com>
2024-05-24 12:17:36 +0900

    Revert "Ajax everything (#35)" (#38)
    This reverts commit 1da638791b9227bb34d90b18fc17b9c597302449.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,11 +1,4 @@
 class Note < ApplicationRecord
   belongs_to :user
   validates :video_url, presence: true
-
-  # after_save :method
-
-  # def method
-  #   # self.transcript = TranscriptGenerator.beautify_transcript(self.transcript)
-  #   # self.save
-  # end
 end
