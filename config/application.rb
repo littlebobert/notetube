d96b16882e65232576bce1d54908f85da5b845b9
Justin Garcia <justin.garcia@gmail.com>
2024-05-24 14:13:53 +0900

    speculative fix for Heroku asset error (#46)

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,7 @@ module Notetube
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.assets.css_compressor = nil
   end
 end
