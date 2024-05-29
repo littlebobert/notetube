7c93c89671c8ccfdcba7d622d28f13034654c3c1
Justin Garcia <justin.garcia@gmail.com>
2024-05-29 12:27:41 +0900

    update quiz prompt

--- app/service/quiz_generator.rb
+++ app/service/quiz_generator.rb
@@ -15,7 +15,7 @@ class QuizGenerator
       'Authorization' => "Bearer #{api_key}"
     }
     
-    prompt = "We are building a website for generating lecture notes from a YouTube lecture video transcript. Please generate a 5 question multiple choice quiz with answers from the following transcript as JSON: #{@note.transcript}"
+    prompt = "We are building a website for generating lecture notes from a YouTube lecture video transcript. Please generate a 5 question multiple choice quiz with answers. Make sure the answer is exactly the same as one of the options. Please return JSON in the form {\"quiz\":[{\"question\":\"Some question\",\"answer\":\"The answer\",\"options\":[\"Some option\"]}. Please use the following transcript as JSON: #{@note.transcript}"
     
     body = {
     model: 'gpt-4o',
