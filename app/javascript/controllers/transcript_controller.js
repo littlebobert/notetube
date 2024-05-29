9d904d197e4580b64dd1ba2d04acd94041711088
Justin Garcia <justin.garcia@gmail.com>
2024-05-29 10:01:47 +0900

    save

--- app/javascript/controllers/transcript_controller.js
+++ app/javascript/controllers/transcript_controller.js
@@ -179,9 +179,9 @@ export default class extends Controller {
   }
   
   share(event) {
+    // Copies the current URL to the clipboard. Copied from https://stackoverflow.com/questions/49618618/copy-current-url-to-clipboard
     var dummy = document.createElement('input'),
     text = window.location.href;
-    
     document.body.appendChild(dummy);
     dummy.value = text;
     dummy.select();
